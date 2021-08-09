cfc2929e20d22d5cb15081a6720612f6e95527da
callor <callor@callor.com>
2021-08-09 16:09:41 +0900

    Naver

--- App02_Library/app/src/main/java/com/callor/library/apdapter/BookAdapter.java
+++ App02_Library/app/src/main/java/com/callor/library/apdapter/BookAdapter.java
@@ -1,24 +1,45 @@
 package com.callor.library.apdapter;
 
-import android.view.View;
+import android.os.Build;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.callor.library.databinding.BookItemViewBinding;
 import com.callor.library.model.BookDTO;
 
 import java.util.List;
 
 public class BookAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<BookDTO> bookList ;
+
+    public BookAdapter(List<BookDTO> bookDTOList) {
+        this.bookList = bookDTOList;
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        BookItemViewBinding bookItemViewBinding
+                = BookItemViewBinding.inflate(
+                        layoutInflater,
+                        parent, false);
+
+        RecyclerView.ViewHolder viewHolder
+                = new BookViewHolder(bookItemViewBinding);
+        return viewHolder;
     }
+
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        BookViewHolder bookHolder = (BookViewHolder) holder;
+        BookDTO bookDTO = bookList.get(position);
+        bookHolder.bookItemView.itemTxtTitle.setText( bookDTO.getTitle() );
     }
     @Override
     public int getItemCount() {
@@ -29,8 +50,25 @@ public class BookAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
      * 각 데이터 item을 표현하기 위한 View 객체 생성하기
      */
     public static class BookViewHolder extends RecyclerView.ViewHolder {
-        public BookViewHolder(@NonNull View itemView) {
-            super(itemView);
+
+        /**
+         * DataBinding이 true 로 되어 있을때
+         * book_item_view.xml layout 파일을 생성하면
+         * 자동으로 선언되는 클래스
+         *
+         * DataBinding을 선언하면
+         * layout.xml에 선언된 Component를 일일히 한개씩 세팅하지 않아도
+         * binding 객체 한개만 세팅하면 나머지는 자동으로 같이 세팅이 된다
+         */
+        public BookItemViewBinding bookItemView;
+        public BookViewHolder(@NonNull BookItemViewBinding bookItemViewBinding) {
+            super(bookItemViewBinding.getRoot());
+            bookItemView = bookItemViewBinding;
         }
+
+        public void bind(BookDTO bookDTO) {
+        }
+
+
     }
 }
