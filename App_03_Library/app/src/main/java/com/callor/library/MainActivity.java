93841152bc29b1231d6bb0774ea571a18dbef5d1
callor <callor@callor.com>
2021-08-11 09:50:54 +0900

    ActionBar

--- App_03_Library/app/src/main/java/com/callor/library/MainActivity.java
+++ App_03_Library/app/src/main/java/com/callor/library/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.callor.library.service.NaverBookService;
 import com.callor.library.service.impl.NaverBookServiceImplV1;
@@ -20,5 +21,18 @@ public class MainActivity extends AppCompatActivity {
         NaverBookService naverBookService
                 = new NaverBookServiceImplV1(recyclerView);
         naverBookService.getBooks("자바");
+    }
+
+    /**
+     * res/menu/menu.xml 파일을 읽어서 화면의
+     * ActionBar에 메뉴 등을 표현할때 사용하는 method
+     *
+     * 이 method는 Android가 App을 화면에 띄울때 자동으로
+     * 호출하여 사용하는 method
+     * 
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
